e6df346db0c05f90a56f1edb38e4bbee5aa32df0
Muhamad Fakhrusy <mfakhrusy@outlook.com>
2017-12-11 17:50:39 +0700

    add new func js non finished

--- freecodecamp/front-end/intermediate-project/random-quote/main.js
+++ freecodecamp/front-end/intermediate-project/random-quote/main.js
@@ -3,20 +3,18 @@ $(document).ready(function() {
 	
 	// ========== NEW QUOTE FUNCTION ==========
 	$(".box-new-button").click(function(event) {
-		$.getJSON('test.json', function(data) {
+		$.getJSON('quote.json', function(data) {
 
 			var quote = data.quote;
 			var quoteLength = quote.length;
 			var arr = [...Array(quoteLength).keys()];
-			console.log(quote);
 			var randomNum = arr[Math.floor(Math.random() *
 				arr.length)];
-
 
 			// First, if .box-new-button is clicked, the
 			// background color is changed
 			color = quote[randomNum].bgColor;			
-			$(".site-background").css("background-color", color);
+			$(".site-background").css({"background-color" : color});
 
 			// we also need to change the quote text
 			quoteText = quote[randomNum].quoteText;
@@ -26,5 +24,11 @@ $(document).ready(function() {
 			quoteAuthor = quote[randomNum].author;
 			$(".box-author").html("<p> - " + quoteAuthor + "</p>");
 		});
+	});
+
+	// ============= SHARE TO TWITTER FUNCTION ==============
+	$(".twitter-button").click(function(event) {
+		// will change the attribute href according to the quote
+		
 	});
 });
