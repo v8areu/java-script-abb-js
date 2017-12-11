238fb18822cfa69810a6862a4e1cdd82929af2f7
Muhamad Fakhrusy <mfakhrusy@outlook.com>
2017-12-11 21:45:49 +0700

    finished barely random quote

--- freecodecamp/front-end/intermediate-project/random-quote/main.js
+++ freecodecamp/front-end/intermediate-project/random-quote/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	// ========== JSON PARSING =========
 	
 	// ========== NEW QUOTE FUNCTION ==========
-	$(".box-new-button").click(function(event) {
+	$(".new-quote-button").click(function(event) {
 		$.getJSON('quote.json', function(data) {
 
 			var quote = data.quote;
@@ -13,22 +13,38 @@ $(document).ready(function() {
 
 			// First, if .box-new-button is clicked, the
 			// background color is changed
-			color = quote[randomNum].bgColor;			
+			var color = quote[randomNum].bgColor;			
 			$(".site-background").css({"background-color" : color});
 
 			// we also need to change the quote text
-			quoteText = quote[randomNum].quoteText;
+			var quoteText = quote[randomNum].quoteText;
 			$(".box-text").html("<h2>" + quoteText + "</h2>");
 
 			// also change the author!
-			quoteAuthor = quote[randomNum].author;
+			var quoteAuthor = quote[randomNum].author;
 			$(".box-author").html("<p> - " + quoteAuthor + "</p>");
 		});
 	});
 
 	// ============= SHARE TO TWITTER FUNCTION ==============
-	$(".twitter-button").click(function(event) {
+	$(".twitter-button").hover(function(event) {
+		// get the quote text
+		var quoteText = $('.box-text h2').text();
+
+		// get the quote author
+		var quoteAuthor = $('.box-author p').text();
+		
 		// will change the attribute href according to the quote
+		var twitterBase = 'https://twitter.com/intent/tweet';
+		var shareText = "?text=\"" + quoteText + "\"";
+		shareText += quoteAuthor;
+		var shareHashTag = "&hashtags=quote";
+
+		var twitterShare = twitterBase;
+		twitterShare += shareText;
+		twitterShare += shareHashTag;
+
+		$('.box-social-button a').attr("href", twitterShare);
 		
 	});
 });
