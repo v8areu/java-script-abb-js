66546f5b6e0654409a9e1afec4cd40b911fa522f
Muhamad Fakhrusy <mfakhrusy@outlook.com>
2017-12-15 14:50:38 +0700

    progress

--- freecodecamp/front-end/intermediate-project/wikipedia-viewer/js/scripts.js
+++ freecodecamp/front-end/intermediate-project/wikipedia-viewer/js/scripts.js
@@ -1,12 +1,39 @@
 $(document).ready(function() {
   // function click for input form to be changed
+  var expanded = false;
   $(".search-icon").click(function(event) {
-    $(".search-icon input").css({
+    
+
+	$(".search-icon input").css({
           "width" : "250px",
           "border-radius" : "30px",
           "transition" : "0.25s",
           "cursor" : "auto",
           "padding" : "0 15px 0 15px"
     });
+	
+	if (!expanded) {
+
+		$(".close-search").toggleClass("close-search-expand");
+		$(".close-search-expand").toggleClass("close-search");
+		expanded = true;
+
+	}
+
   });
+
+	$(".close-search").click(function(event) {
+	
+		expanded = false;
+
+		$(".search-icon input").css({
+			"width" : "34px",
+			"height" : "34px",
+			"margin" : "10px",
+			"transition" : "0.25s",
+			"cursor" : "auto",
+		});
+
+
+	});	
 });
