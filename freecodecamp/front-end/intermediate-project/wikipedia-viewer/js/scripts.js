807ce9c11e6ab1cc519b6177d467261ec288c6f1
Muhamad Fakhrusy <v8areu@gmail.com>
2017-12-15 08:59:39 +0700

    edit things

--- freecodecamp/front-end/intermediate-project/wikipedia-viewer/js/scripts.js
+++ freecodecamp/front-end/intermediate-project/wikipedia-viewer/js/scripts.js
@@ -8,14 +8,5 @@ $(document).ready(function() {
           "cursor" : "auto",
           "padding" : "0 15px 0 15px"
     });
-
-    $(".search-icon").addClass("close-input");
-  });
-
-  $(".search-icon").click(function(event) {
-    $("search-icon input").css({
-      "width" : "30px",
-      "border-radius" : "50%"
-    });
   });
 });
