3bbb71f46ad126114471bddd05ce3d8394f90ac5
Muhamad Fakhrusy <mfakhrusy@outlook.com>
2017-12-16 14:31:51 +0700

    now doing wiki api

--- freecodecamp/front-end/intermediate-project/wikipedia-viewer/js/scripts.js
+++ freecodecamp/front-end/intermediate-project/wikipedia-viewer/js/scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	// function click for input form to be changed
 	let expanded = false;
+	let onTop = false;
 	
 	// expand magnifier
 	$(".search-icon input").click(function(event) {
@@ -29,14 +30,61 @@ $(document).ready(function() {
 
 		$(".close-search-expand").toggleClass("close-search");
 		$(".close-search").toggleClass("close-search-expand");
+
+		if (onTop) {
+			$(".middle").css({"display": "table-cell"});
+
+			$(".search-info")
+				.append("<p> Click icon to search </p>");
+			onTop = false;
+		}
+
+		// empty the input field
+		$("#search-query").val('');
 	});	
+
+	// make javascript form not reloading the page when pressing 
+	// enter
+	$(function() {
+		$("#search-query-form").submit(function() {
+			return false;
+		});
+	});
 
 	let value;
 	// get value from input box
 	$("#search-query").keyup(function(e) {
 		if (e.keyCode === 13) {
 			value = $("#search-query").val();
-			alert(value);
+			/*alert(value);*/
+			$(".middle").css({"display": "block"});
+
+			// delete the Click icon to search paraghraph
+			$(".search-info > p").remove();
+			onTop = true;
+
+			// add the container div block
+			/*
+			jQuery("<div/>", {
+				id: 'container-query-result',
+				class: 'container'
+			}).appendTo(".outer");
+
+			// add the row div block
+			jQuery("<div/>", {
+				id: 'row-query-result',
+				class: 'row'
+			}).appendTo("#container-query-result");
+
+			// add grid block -> TODO loop it by the API
+			jQuery("<div/>", {
+				class: "col-lg-12"
+			}).addClass("query-result-box")
+			.appendTo("#row-query-result");
+			*/
 		}
 	});
+
+	// wikipedia API function goes here
+	
 });
