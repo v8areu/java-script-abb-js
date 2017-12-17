5b8680da79777efc64560b6e95a755b69d353dbd
Muhamad Fakhrusy <v8areu@gmail.com>
2017-12-17 09:18:48 +0700

    wiki api done, display to html now

--- freecodecamp/front-end/intermediate-project/wikipedia-viewer/js/scripts.js
+++ freecodecamp/front-end/intermediate-project/wikipedia-viewer/js/scripts.js
@@ -2,18 +2,18 @@ $(document).ready(function() {
 	// function click for input form to be changed
 	let expanded = false;
 	let onTop = false;
-	
+
 	// expand magnifier
 	$(".search-icon input").click(function(event) {
 
 		if (!expanded) {
 
 			$(".search-icon").toggleClass("search-icon-expand");
-			$(".search-icon-expand").toggleClass("search-icon");	
+			$(".search-icon-expand").toggleClass("search-icon");
 
 			$(".close-search").toggleClass("close-search-expand");
 			$(".close-search-expand").toggleClass("close-search");
-			
+
 			expanded = true;
 
 		}
@@ -22,7 +22,7 @@ $(document).ready(function() {
 
 	// un-expand magnifier
 	$(".close-search").click(function(event) {
-	
+
 		expanded = false;
 
 		$(".search-icon-expand").toggleClass("search-icon");
@@ -41,9 +41,9 @@ $(document).ready(function() {
 
 		// empty the input field
 		$("#search-query").val('');
-	});	
+	});
 
-	// make javascript form not reloading the page when pressing 
+	// make javascript form not reloading the page when pressing
 	// enter
 	$(function() {
 		$("#search-query-form").submit(function() {
@@ -64,7 +64,7 @@ $(document).ready(function() {
 			onTop = true;
 
 			// add the container div block
-			/*
+
 			jQuery("<div/>", {
 				id: 'container-query-result',
 				class: 'container'
@@ -76,15 +76,52 @@ $(document).ready(function() {
 				class: 'row'
 			}).appendTo("#container-query-result");
 
+/*
 			// add grid block -> TODO loop it by the API
 			jQuery("<div/>", {
 				class: "col-lg-12"
 			}).addClass("query-result-box")
 			.appendTo("#row-query-result");
-			*/
-		}
+
+*/
+			// add <ul> tag
+			jQuery("<ul/>", {
+				id: 'ul-query-result',
+			}).appendTo("#row-query-result");
+
+			// loop each wikipedia API
+			// JSON URL
+			var wikiBASE = 'https://en.wikipedia.org';
+			var wikiAPI = "/w/api.php?action=query&origin=*&format=json&prop=extracts&generator=search&exsentences=1&exintro=1&explaintext=1&excontinue=1&gsrsearch=";
+			var searchQuery = $("#search-query").val();
+
+      // this is commented because cross origin probleme, need
+      // to come up with another method (below this one)
+      $.ajax({
+        url: wikiBASE + wikiAPI + searchQuery,
+        datatype: 'jsonp',
+        success: function(wikiData, dataStatus, jqXHR) {
+          console.log(wikiData.query);
+          //error: function(jqXHR, dataStatus, errorThrown) {
+          //    alert("not ok!");
+          //}
+        }
+      });
+
+/*
+$.getJSON('http://whateverorigin.org/get?url=' +
+    encodeURIComponent(wikiBASE + wikiAPI + searchQuery) + '&callback=?',
+    function (data) {
+        //console.log("> ", data);
+
+        //If the expected response is text/plain
+        // $("#viewer").html(data.contents);
+
+        //If the expected response is JSON
+        var response = $.parseJSON(data.contents);
+});
+*/
+		} // if close bracket
 	});
 
-	// wikipedia API function goes here
-	
 });
