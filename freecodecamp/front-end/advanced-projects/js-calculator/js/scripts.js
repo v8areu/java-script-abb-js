305e65b7f5dc9ede3725d432bd1daf08eda03c1e
Muhamad Fakhrusy <mfakhrusy@outlook.com>
2017-12-26 15:20:43 +0700

    edit some calc

--- freecodecamp/front-end/advanced-projects/js-calculator/js/scripts.js
+++ freecodecamp/front-end/advanced-projects/js-calculator/js/scripts.js
@@ -1,4 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
+  // function to capture when user click button
+  let calcButtons = document.getElementsByTagName("button");
+  let calcButtonsParentID = []; 
+  for (let calcButton of calcButtons) {
+    calcButtonsParentID.push(calcButton.parentNode.id);
+  }
+
+  // get the upper and lower screen paraghraphs
+  let screenUpperP = document.querySelector("#screen-upper p");
+  let screenLowerP = document.querySelector("#screen-lower p");
 
 });
