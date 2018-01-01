610536823bb02da6bb33cfe87f90835538cc74be
Muhamad Fakhrusy <mfakhrusy@outlook.com>
2018-01-01 07:32:36 +0700

    calc done now doing button CE and fix bugs

--- freecodecamp/front-end/advanced-projects/js-calculator/js/scripts.js
+++ freecodecamp/front-end/advanced-projects/js-calculator/js/scripts.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   // define digit limit
   const digitLimit = 8;
+  const digitLimitLower = 20;
 
   // get the upper and lower screen for convenience
   let screenUpper = document.getElementById("screen-upper");
@@ -12,16 +13,28 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // default is false
   let isDecimal = false;
 
+  let isResultDecimal = false;
   // initialized means that it contains the initial 0
   // it should be true from the start since
   // the 0 is there if the calculator is on
   let isInitialized = true;
 
+  // check if it's the first number
+  let isFirstNumber = true;
+    
+  // the user can only press math button if a valid number is already appear
+  let isMathOK = false;
+
   // need to save the variable before computation
   // e.g 3x5 (need to save 3 WHEN pressed x so we can just
   // compute it right away when we pressed 5)
-  let numBeforeMath;
-  let isMathOK = false;
+  let numBeforeMath = 0;
+  let lastMathProcess = "";
+
+  // isMathProcess is here because we want to delete
+  // the math process on upper screen and I guess
+  // we need another variable for this
+  let isMathProcess = false;
 
   // function to capture when user click button
   // We will get the parent-id of each button
@@ -42,22 +55,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // get the class of each math button
   let buttonsMath = document.getElementsByClassName("button-math");
 
-  // add default value
-
   // loop the buttonsNumber for it to be appearing on
   // the screenlower
   let tempDigit = 0;
   for (let buttonNumber of buttonsNumber) {
     buttonNumber.addEventListener("click", function(e) {
 
-      let tempButtonNumberTextNode = buttonNumber.firstElementChild.firstChild.cloneNode(true);
-      let tempButtonNumberTextNode2 = buttonNumber.firstElementChild.firstChild.cloneNode(true);
+      //let tempButtonNumberTextNode = buttonNumber.firstElementChild.firstChild.cloneNode(true);
+      //let tempButtonNumberTextNode2 = buttonNumber.firstElementChild.firstChild.cloneNode(true);
 
-      // first, make sure that the digit appear less than 
-      
+      // first, make sure that the digit appear less than  digitLimit
       if (tempDigit < digitLimit) {
 
-        console.log(isMathOK);
         // remove the initial value (0) or the math process
         if (isInitialized && this.parentNode.id !== "calc-zero") {
           screenUpper.removeChild(screenUpper.firstElementChild);
@@ -65,7 +74,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
           isInitialized = false;
         }
 
-        if (isMathOK) {
+        if (isMathProcess) {
           screenUpper.removeChild(screenUpper.firstElementChild);
         }
 
@@ -100,26 +109,35 @@ document.addEventListener("DOMContentLoaded", function(event) {
           // e.g we can't write 000000 if we keep pressing zero
           // the zero can only be used if it's after any other 
           // value like 200 or 312300
-          if (screenUpper.firstElementChild.firstChild.nodeValue != 0 || !isInitialized) {
-            screenUpper.firstElementChild.appendChild(tempButtonNumberTextNode);
+          // the (isMathProcess || isDecimal) is needed because
+          // 1. when the math button is pressed, we need to still comply with the above rule (no writing 2x0000)
+          // 2. if we just add isMathProcess, when user pressed the decimal, they can't press
+          // zero after the decimal, we need that to compromise it.
+          // e.g we need to write like 2x0.05 (if no isDecimal we will stuck when write 2x0. -> press zero won't have any effect)
+          if (!isInitialized && (isMathProcess || isDecimal || isMathOK)) {
+            screenUpper.firstElementChild.appendChild(buttonNumber.firstElementChild.firstChild.cloneNode(true));
             tempDigit++;
             isMathOK = true;
           }
 
-          if (screenLower.firstElementChild.firstChild.nodeValue != 0 || !isInitialized) {
-            screenLower.firstElementChild.appendChild(tempButtonNumberTextNode2);
+          if (!isInitialized && (isMathProcess || isDecimal || isMathOK)) {
+            screenLower.firstElementChild.appendChild(buttonNumber.firstElementChild.firstChild.cloneNode(true));
+
+            // the isMathProcess should be false here because if we put it above, the
+            // screenLower won't print the 0
+            isMathProcess = false;
           }
 
         } else {
-        
-          screenUpper.firstElementChild.appendChild(tempButtonNumberTextNode);
-          screenLower.firstElementChild.appendChild(tempButtonNumberTextNode2);
+
+          screenUpper.firstElementChild.appendChild(buttonNumber.firstElementChild.firstChild.cloneNode(true));
+          screenLower.firstElementChild.appendChild(buttonNumber.firstElementChild.firstChild.cloneNode(true));
 
           tempDigit++;
           isMathOK = true;
+          isMathProcess = false;
         
         }
-
       }
     });
   }
@@ -139,31 +157,31 @@ document.addEventListener("DOMContentLoaded", function(event) {
       screenLower.appendChild(document.createElement("p"));
     }
 
-      // need to check if it's decimal or not
-      // if it's decimal already, the button won't
-      // have any additional effect
-      // if it's not, the decimal will appear (notated by dot)
-      if (!isDecimal) {
-        isDecimal = true;
-        let tempDotTextNode = document.createTextNode(".");
-        let tempDotTextNode2 = document.createTextNode(".");
-        screenUpper
-          .firstElementChild.appendChild(tempDotTextNode);
-        screenLower
-          .firstElementChild.appendChild(tempDotTextNode2);
+    // need to check if it's decimal or not
+    // if it's decimal already, the button won't
+    // have any additional effect
+    // if it's not, the decimal will appear (notated by dot)
+    // addition: add isMathProcess because
+    // we must not write 3x. -> decimal after math process
+    if (!isDecimal && !isMathProcess) {
+      isDecimal = true;
+      let tempDotTextNode = document.createTextNode(".");
+      let tempDotTextNode2 = document.createTextNode(".");
+      screenUpper
+        .firstElementChild.appendChild(tempDotTextNode);
+      screenLower
+        .firstElementChild.appendChild(tempDotTextNode2);
+      // the initialization should become false because
+      // it's not initialized anymore
+      isInitialized = false;
 
-        // the initialization should become false because
-        // it's not initialized anymore
-        isInitialized = false;
-
-        // the digit should be still be 8 if zero
-        // is the first digit
-        // e.g use 0.1111111 rather than 0.11111111
-        if (screenUpper.firstElementChild.firstChild.nodeValue == 0) {
-          tempDigit++;
-        }
+      // the digit should be still be 8 if zero
+      // is the first digit
+      // e.g use 0.1111111 rather than 0.11111111
+      if (screenUpper.firstElementChild.firstChild.nodeValue == 0) {
+        tempDigit++;
       }
-
+    }
   });
 
   // EQUATION FUNCTION
@@ -186,16 +204,103 @@ document.addEventListener("DOMContentLoaded", function(event) {
       }
       tempNumValue = Number(tempNumValue);
 
-      numBeforeMath = tempNumValue;
-
       // special case -> equal
       if (buttonMath.parentNode.id === "calc-equal") {
-        let tempMath = "="
-        
+        let tempMath = "=";
+
+        if (isMathOK) {
+
+          if (lastMathProcess === "/") {
+
+            numBeforeMath = numBeforeMath / tempNumValue;
+
+          } else if (lastMathProcess === "+") {
+
+            numBeforeMath = numBeforeMath + tempNumValue;
+            
+          } else if (lastMathProcess === "-") {
+
+            numBeforeMath = numBeforeMath - tempNumValue;
+
+          } else if (lastMathProcess === "x") {
+            
+            numBeforeMath = numBeforeMath * tempNumValue;
+
+          } else {
+
+            // this is for the first number
+            numBeforeMath = tempNumValue;
+
+          }
+
+          // remove screen upper and change it to the math process
+          screenUpper.removeChild(screenUpper.firstElementChild);
+
+          // display the equal IF the user has input any number
+          screenUpper.appendChild(document.createElement("p"));
+          screenLower.firstElementChild.appendChild(document.createTextNode(tempMath));
+
+          // check if it's decimal (contains . (dot))
+          if (String(numBeforeMath).indexOf(".") !== -1) {
+            isResultDecimal = true;
+          }
+
+          if (String(numBeforeMath).length > 4 && isResultDecimal) {
+            screenUpper.firstElementChild.appendChild(document.createTextNode(numBeforeMath.toFixed(3)));
+            screenLower.firstElementChild.appendChild(document.createTextNode(numBeforeMath.toFixed(3)));
+          } else {
+            screenUpper.firstElementChild.appendChild(document.createTextNode(numBeforeMath));
+            screenLower.firstElementChild.appendChild(document.createTextNode(numBeforeMath));
+          }
+
+          // isMathProcess set to true too
+          // isMathProcess = true;
+          
+          // variable re-initialization
+          isDecimal = false;
+          isInitialized = true;
+          isMathOK = false;
+          isMathProcess = false;
+          isFirstNumber = true;
+          tempDigit = 0;
+          numBeforeMath = 0;
+          lastMathProcess = "";
+          isResultDecimal = false;
+        }
+
       } else if (buttonMath.parentNode.id === "calc-multiply") {
         let tempMath = "x";
         
         if (isMathOK) {
+
+          if (lastMathProcess === "x") {
+
+            numBeforeMath = numBeforeMath * tempNumValue;
+
+          }
+          else if (lastMathProcess === "/") {
+
+            numBeforeMath = numBeforeMath / tempNumValue;
+
+          } else if (lastMathProcess === "+") {
+
+            numBeforeMath = numBeforeMath + tempNumValue;
+            
+          } else if (lastMathProcess === "-") {
+
+            numBeforeMath = numBeforeMath - tempNumValue;
+
+          } else if (lastMathProcess === "=") {
+
+          } else {
+
+            // this is for the first number
+            numBeforeMath = tempNumValue;
+
+          }
+
+          // change lastMathProcess
+          lastMathProcess = tempMath;
 
           // remove screen upper and change it to the math process
           screenUpper.removeChild(screenUpper.firstElementChild);
@@ -203,43 +308,154 @@ document.addEventListener("DOMContentLoaded", function(event) {
           // display the cross IF the user has input any number
           screenUpper.appendChild(document.createElement("p"));
           screenUpper.firstElementChild.appendChild(document.createTextNode(tempMath));
-
           screenLower.firstElementChild.appendChild(document.createTextNode(tempMath));
 
+          isMathProcess = true;
+          tempDigit = 0;
+          isDecimal = false;
         }
-       
 
       } else if (buttonMath.parentNode.id === "calc-division") {
         let tempMath = "/";
 
-        /* remove screen upper and change it to the math process
-        screenUpper.removeChild(screenUpper.firstElementChild);
+        if (isMathOK) {
 
-        // display the division
-        screenUpper.appendChild(document.createElement("p"));
-        screenUpper.firstElementChild.appendChild(document.createTextNode(tempMath));
-        */
+          if (lastMathProcess === "x") {
+
+            numBeforeMath = numBeforeMath * tempNumValue;
+
+          } else if (lastMathProcess === "/") {
+            
+            numBeforeMath = numBeforeMath / tempNumValue;
+
+          } else if (lastMathProcess === "+") {
+
+            numBeforeMath = numBeforeMath + tempNumValue;
+            
+          } else if (lastMathProcess === "-") {
+
+            numBeforeMath = numBeforeMath - tempNumValue;
+
+          } else if (lastMathProcess === "=") {
+
+          } else {
+
+            // this is for the first number
+            numBeforeMath = tempNumValue;
+
+          }
+
+          // change lastMathProcess
+          lastMathProcess = tempMath;
+
+          // remove screen upper and change it to the math process
+          screenUpper.removeChild(screenUpper.firstElementChild);
+
+          // display the cross IF the user has input any number
+          screenUpper.appendChild(document.createElement("p"));
+          screenUpper.firstElementChild.appendChild(document.createTextNode(tempMath));
+          screenLower.firstElementChild.appendChild(document.createTextNode(tempMath));
+
+          isMathProcess = true;
+          tempDigit = 0;
+          isDecimal = false;
+
+        }
+
       } else if (buttonMath.parentNode.id === "calc-addition") {
         let tempMath = "+";
 
-        /* remove screen upper and change it to the math process
-        screenUpper.removeChild(screenUpper.firstElementChild);
+        if (isMathOK) {
 
-        // display the addition
-        screenUpper.appendChild(document.createElement("p"));
-        screenUpper.firstElementChild.appendChild(document.createTextNode(tempMath));
-        */
+          if (lastMathProcess === "x") {
+
+            numBeforeMath = numBeforeMath * tempNumValue;
+
+          } else if (lastMathProcess === "/") {
+
+            numBeforeMath = numBeforeMath / tempNumValue;
+            
+          } else if (lastMathProcess === "+") {
+
+            numBeforeMath = numBeforeMath + tempNumValue;
+          
+          } else if (lastMathProcess === "-") {
+
+            numBeforeMath = numBeforeMath - tempNumValue;
+
+          } else if (lastMathProcess === "=") {
+
+          } else {
+
+            // this is for the first number
+            numBeforeMath = tempNumValue;
+
+          }
+
+          // change lastMathProcess
+          lastMathProcess = tempMath;
+
+          // remove screen upper and change it to the math process
+          screenUpper.removeChild(screenUpper.firstElementChild);
+
+          // display the cross IF the user has input any number
+          screenUpper.appendChild(document.createElement("p"));
+          screenUpper.firstElementChild.appendChild(document.createTextNode(tempMath));
+          screenLower.firstElementChild.appendChild(document.createTextNode(tempMath));
+
+          isMathProcess = true;
+          tempDigit = 0;
+          isDecimal = false;
+
+        }
+
 
       } else if (buttonMath.parentNode.id === "calc-substract") {
         let tempMath = "-";
 
-        /* remove screen upper and change it to the math process
-        screenUpper.removeChild(screenUpper.firstElementChild);
+        if (isMathOK) {
 
-        // display the substraction
-        screenUpper.appendChild(document.createElement("p"));
-        screenUpper.firstElementChild.appendChild(document.createTextNode(tempMath));
-        */
+          if (lastMathProcess === "x") {
+
+            numBeforeMath = numBeforeMath * tempNumValue;
+
+          } else if (lastMathProcess === "/") {
+
+            numBeforeMath = numBeforeMath / tempNumValue;
+            
+          } else if (lastMathProcess === "+") {
+
+            numBeforeMath = numBeforeMath + tempNumValue;
+
+          } else if (lastMathProcess === "-") {
+
+            numBeforeMath = numBeforeMath - tempNumValue;
+          
+          } else if (lastMathProcess === "=") {
+
+          } else {
+
+            // this is for the first number
+            numBeforeMath = tempNumValue;
+
+          }
+
+          // change lastMathProcess
+          lastMathProcess = tempMath;
+
+          // remove screen upper and change it to the math process
+          screenUpper.removeChild(screenUpper.firstElementChild);
+
+          // display the cross IF the user has input any number
+          screenUpper.appendChild(document.createElement("p"));
+          screenUpper.firstElementChild.appendChild(document.createTextNode(tempMath));
+          screenLower.firstElementChild.appendChild(document.createTextNode(tempMath));
+
+          isMathProcess = true;
+          tempDigit = 0;
+          isDecimal = false;
+
+        }
       }
     });
   }
@@ -247,16 +463,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // button AC function
   let buttonAC = buttonsParentID["calc-ac"];
   buttonAC.addEventListener("click", function(e) {
-
-    // make isDecimal false again
-    isDecimal = false;
-
-    // initialize
-    isInitialized = true;
-
-    // isMathOK should be false because of initialization
-    isMathOK = false;
-
+    
     // remove the nodes inside paraghraph of screen upper 
     // and screen lower
     // we need to remove THOSE + the paraghraph tag inside
@@ -278,10 +485,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
     screenUpper.appendChild(document.createElement("p"));
     screenUpper.firstElementChild.appendChild(document.createTextNode("0"));
 
+    // variable re-initialization
+    isDecimal = false;
+    isInitialized = true;
+    isMathOK = false;
+    isMathProcess = false;
+    isFirstNumber = true;
     tempDigit = 0;
+    numBeforeMath = 0;
+    lastMathProcess = "";
+    isResultDecimal = false;
   });
 
-
-  // misc functions
-  
+  // button CE function
+  let buttonCE = buttonsParentID["calc-ce"];
+  buttonCE.addEventListener("click", function(e) {
+        
+  });
 });
